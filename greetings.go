4fb568c50bb1ce455ca119d20f449aaa3e12e300
Yaw Boakye-Owusu <yaw.owusu@hotmail.com>
2021-06-11 10:49:15 +0300

    added error check

--- greetings.go
+++ greetings.go
@@ -1,13 +1,18 @@
 package greetgo
 
 import (
+	"errors"
 	"fmt"
 )
 
 // Hello returns a greeting for the named person.
-func Hello(name string) string {
+func Hello(name string) (string, error) {
+	if name == "" {
+		return "", errors.New("empty name")
+	}
+
 	// Return a greeting that embeds the name in a messager
 	message := fmt.Sprintf("Hi, %v. Welcome!", name)
-	return message
+	return message, nil
 }
 
